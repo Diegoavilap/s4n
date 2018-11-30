ee424b2ef3e13ea47abe6bc5cfd867865f89271e
Lawliet02 <diegoavilapava@hotmail.com>
2018-11-29 22:26:56 -0500

    Added Unit Tests for handling errors and combine CompletableFuture

--- WeekTwo/test/com/diegoavilap/two/CompletableFutureTest.java
+++ WeekTwo/test/com/diegoavilap/two/CompletableFutureTest.java
@@ -2,7 +2,9 @@ package com.diegoavilap.two;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -28,7 +30,7 @@ public class CompletableFutureTest {
     
     @AfterEach
     public void shutDown() throws InterruptedException {
-        Sleep.sleepSeconds(2);
+        Sleep.sleepSeconds(5);
         executor.shutdown();
         executor.awaitTermination(10, TimeUnit.SECONDS);
     }
@@ -72,18 +74,19 @@ public class CompletableFutureTest {
     @Test
     public void whenSupplyAsyncIsCalled_thenTheCompletableFuture() throws InterruptedException, ExecutionException {
     	CompletableFuture<String> futureSupplyAsync = CompletableFuture.supplyAsync(() -> {
-    	    LOGGER.info("Comenzando supplyAsync...");
-    	    LOGGER.info("Terminado supplyAsync!");
+    	    LOGGER.info("Pasando por el supplyAsync...");
     	    return "S4N";
     	}, executor);
     	assertEquals("S4N", futureSupplyAsync.get());
     }
     
+    
+    //whenCompleteAsync() ---> Callback
+    @Disabled
     @Test
-    public void whenCompleteAsyncIsCalled_thenIsCorrected() {
+    public void whenCompleteAsyncIsCalled_thenIsCorrected() throws InterruptedException, ExecutionException {
     	CompletableFuture<String> futureSupplyAsync = CompletableFuture.supplyAsync(() -> {
-    	    LOGGER.info("Comenzando supplyAsync...");
-    	    LOGGER.info("Terminado supplyAsync!");
+    	    LOGGER.info("Pasando por el supplyAsync...");
     	    return "S4N";
     	}, executor);
     	
@@ -92,7 +95,176 @@ public class CompletableFutureTest {
     	// el segundo parametro es el exception
     	futureSupplyAsync.whenCompleteAsync((s, e) -> LOGGER.info("Resultado supplyAsync: " + s),executor);
     	LOGGER.info("Terminado main thread");
+    	assertEquals("S4N", futureSupplyAsync.get());
+    }
+    
+    // --- Processing Results of Asynchronous Computations --
+    
+    //thenApplyAsync --> Map();
+    @Disabled
+    @Test
+    public void whenThenApplyAsyncIsCalled_thenCorrect() throws InterruptedException, ExecutionException {
+    	CompletableFuture<String> futureAsync = CompletableFuture.supplyAsync(() -> {
+    	    System.out.println("Pasando por supplyAsync ...");
+    	    return "s4n";
+    	}, executor);
+    	 
+    	CompletableFuture<String> futureApply = futureAsync.thenApplyAsync(s -> {
+    	    System.out.println("Pasando por thenApplyAsync...");
+    	    return s.toUpperCase();
+    	}, executor);
+    	 
+    	//futureApply.whenCompleteAsync((s, e) -> System.out.println("Resultado applyAsync: " + s), executor);
+    	assertEquals("S4N", futureApply.get());
+    }
+    
+    //thenAcceptAsync
+    @Disabled
+    @Test
+    public void whenThenAcceptAsyncIsCalled_thenCorrect() throws InterruptedException, ExecutionException {
+    	// thenAccept
+    	CompletableFuture<String> futureAsync = CompletableFuture.supplyAsync(() -> {
+    		LOGGER.info("Pasando por el supplyAsync...");
+    		LOGGER.info("Running method1... thread id: " + Thread.currentThread().getId());
+    	    Sleep.sleepSeconds(2);
+    	    return "S4N";
+    	}, executor);
+    	 
+    	futureAsync.thenAcceptAsync(s -> {
+    	    LOGGER.info("Pasando por el thenAccept...");
+    	    LOGGER.info("corriendo en el thread id: " + Thread.currentThread().getId());
+    	    Sleep.sleepSeconds(2);
+    	    LOGGER.info("Este es el resultado del thenAcceptAsync(): {}", s);
+    	}, executor);
+    	
+    	assertEquals("S4N", futureAsync.get());
     }
 
+	// thenRun
+    @Disabled
+    @Test
+    public void whenThenRunAsyncIsCalled_thenIsCorrect() {
+    	CompletableFuture<Void> futureRun = CompletableFuture.runAsync(() -> {
+    	    LOGGER.info("Comenzando runAsync");
+    	    LOGGER.info("corriendo en el thread id: " + Thread.currentThread().getId());
+    	}, executor);
+    	 
+    	futureRun.thenRunAsync(() -> {
+    	    LOGGER.info("Comenzando thenRunAsync...");
+    	    LOGGER.info("corriendo en el thread id: " + Thread.currentThread().getId());
+    	}, executor);
+    	Sleep.sleepSeconds(1);
+    	assertTrue(futureRun.isDone());
+    }
+    
+    //Handling Errors
+    @Disabled
+	@Test
+	public void whenExceptionally_thenIsCorrect() throws InterruptedException, ExecutionException {
+		CompletableFuture<String> futureAsync = CompletableFuture.supplyAsync(() -> {
+		    LOGGER.info("Pasando por supplyAsync...");
+		    throw new RuntimeException("Error en el futuro");
+		}, executor);
+		 
+		CompletableFuture<String> futureEx = futureAsync.exceptionally(e -> {
+		    LOGGER.error("Resultado con excepción!!", e);
+		    return "Mensaje de Error";
+		});
+		 
+		futureEx.whenCompleteAsync((s, e) -> LOGGER.info("Resultado futureEx: {}", s), executor);
+		
+		//------------//
+		assertEquals("Mensaje de Error", futureEx.get());
+	}
 	
+    //handledAsync()
+    @Disabled
+	@Test
+	public void whenHandledFutuure_thenCorrect() throws InterruptedException, ExecutionException {
+		CompletableFuture<String> futureAsync = CompletableFuture.supplyAsync(() -> {
+		    LOGGER.info("Pasando por supplyAsync...");
+		    throw new RuntimeException("Error en el futuro");
+		}, executor);
+		 
+		CompletableFuture<String> handledFuture = futureAsync.handleAsync((s, e) -> {
+		    if (e != null) {
+		        LOGGER.error("Resultado con excepción!!", e);
+		        return "Mensaje de Error";
+		    } else {
+		        LOGGER.info("Resultado: {}", s);
+		        return s;
+		    }
+		}, executor);
+		
+		handledFuture.whenCompleteAsync((s, e) -> LOGGER.info("Resultado handle: {}", s), executor);
+		assertEquals("Mensaje de Error", handledFuture.get());
+	}
+    //whenCompleteAsync() for handling errors
+    @Disabled
+    @Test
+    public void whenCompleteAsync_thenCorrect() throws InterruptedException, ExecutionException {
+    	CompletableFuture<String> futureAsync = CompletableFuture.supplyAsync(() -> {
+    	    LOGGER.info("Pasando por supplyAsync with exception...");
+    	    throw new RuntimeException("Error en el futuro");
+    	}, executor);
+    	 
+    	futureAsync.whenCompleteAsync((s, e) -> {
+    	    if (e != null) {
+    	        LOGGER.error("Resultado con excepción!!", e);
+    	    } else {
+    	        LOGGER.info("Resultado applyAsync: {}", s);
+    	    }
+    	}, executor);
+    	assertThrows(ExecutionException.class, () -> {
+    		futureAsync.get();
+	    }); 
+    }
+    
+    //thenComposeAsync()
+    @Disabled
+    @Test
+    public void whenThenComposeAsync_thenCorrect() throws InterruptedException, ExecutionException {
+    	CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+    	    LOGGER.info("Pasando por supplyAsync...");
+    	    return "Colaboreme profe no sea así";
+    	}, executor);
+    	 
+    	CompletableFuture<String> fCompose =
+    	        future1.thenComposeAsync(s -> CompletableFuture.supplyAsync(() -> {
+    	                    LOGGER.info("Pasando por thenCompose...");
+    	                    return s.concat(", le doy mil pesos");
+    	                }, executor),
+    	                executor);
+    	 
+    	fCompose.whenCompleteAsync((s, e) -> LOGGER.info("Resultado thenCompose: {}", s),
+    	        executor);
+    	
+    	assertEquals("Colaboreme profe no sea así, le doy mil pesos", fCompose.get());
+    }
+    
+    @Test
+    public void givenTwoCompletableFuture_whenThenCombineAsync_thenANewCompletableFuture() throws InterruptedException, ExecutionException {
+    	CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+    	    LOGGER.info("Creando el future1 con supplyAsync...");
+    	    return "Terminado";
+    	}, executor);
+    	 
+    	CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+    	    LOGGER.info("Creando el  future2 con supplyAsync...");
+    	    return "Terminado other";
+    	}, executor);
+    	 
+    	CompletableFuture<String> fCombine =
+    	        future1.thenCombineAsync(future2, (s1, s2) -> {
+    	            LOGGER.info("En el thenCombine, recibidos los resultados: {}, {}", s1, s2);
+    	            return s1 + s2;
+    	        }, executor);
+    	 
+    	fCombine.whenCompleteAsync((s, e) -> LOGGER.info("Resultado thenCombine: {}", s),
+    	        executor);
+    	assertEquals("TerminadoTerminado other", fCombine.get());
+    }
+    
+    
+    
 }
